d89917a3156aa06325d50d29f9f17e615d5c9c15
yyang12302 <100991505+yyang12302@users.noreply.github.com>
2024-07-03 05:59:20 +0800

    Update reading_the_chain.py

--- reading_the_chain.py
+++ reading_the_chain.py
@@ -65,7 +65,7 @@ def is_ordered_block(w3, block_num):
     #YOUR CODE HERE
 	for transaction_hash in block['transactions']:
 		transaction = w3.eth.get_transaction(transaction_hash)
-        	gasPrice = transaction['gasPrice']
+		gasPrice = transaction['gasPrice']
         	if 'maxPriorityFeePerGas' in transaction:
             		gasPrice = transaction['maxPriorityFeePerGas']
         	if gasPrice > lastGP:
