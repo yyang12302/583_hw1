4587b890f3aa6d5ee076e2a59404cc7d9b16a926
yyang12302 <100991505+yyang12302@users.noreply.github.com>
2024-07-03 04:44:33 +0800

    Create get_ape_info.py

---
+++ get_ape_info.py
@@ -0,0 +1,67 @@
+from web3 import Web3
+from web3.contract import Contract
+from web3.providers.rpc import HTTPProvider
+import requests
+import json
+import time
+
+bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
+contract_address = Web3.toChecksumAddress(bayc_address)
+
+#You will need the ABI to connect to the contract
+#The file 'abi.json' has the ABI for the bored ape contract
+#In general, you can get contract ABIs from etherscan
+#https://api.etherscan.io/api?module=contract&action=getabi&address=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D
+with open('/home/codio/workspace/abi.json', 'r') as f:
+	abi = json.load(f) 
+
+############################
+#Connect to an Ethereum node
+api_url = "https://api.etherscan.io/api?module=contract&action=getabi&address=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"  #YOU WILL NEED TO TO PROVIDE THE URL OF AN ETHEREUM NODE
+provider = HTTPProvider(api_url)
+web3 = Web3(provider)
+
+
+def get_from_ipfs(cid,content_type="json"):
+	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
+	#YOUR CODE HERE	
+	gateway="https://api.pinata.cloud/pinning/pinFileToIPFS"
+	headers={
+		'pinata_api_key':'6bf509970b4e9c3ecee6'
+		'pinata_secret_api_key':'d935f96f288a65d6fd2b5984af63efa47d6022cc03cba8aeebefd65a330fa9a6'
+
+	}
+	response = requests.get(gateway, headers=headers)
+	print(response.json())
+	if content type ="json":
+		data = response.json()
+	else:
+		data = response.content
+
+	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
+	return data
+
+def get_ape_info(apeID):
+	assert isinstance(apeID,int), f"{apeID} is not an int"
+	assert 1 <= apeID, f"{apeID} must be at least 1"
+
+	data = {'owner': "", 'image': "", 'eyes': "" }
+	
+	#YOUR CODE HERE
+	owner = contract.functions.ownerOf(apeID).call()
+  data['owner'] = owner
+
+  uri = contract.functions.tokenURI(apeID).call()
+  bayc_cid = uri.replace('ipfs://', '')
+  content_data = get_from_ipfs(bayc_cid)
+
+  data['image'] = content_data['image']
+
+  for attr in content_data['attributes']:
+		if attr['trait_type'] == 'Eyes':
+			data['eyes'] = attr['value']
+
+	assert isinstance(data,dict), f'get_ape_info{apeID} should return a dict' 
+	assert all( [a in data.keys() for a in ['owner','image','eyes']] ), f"return value should include the keys 'owner','image' and 'eyes'"
+	return data
+
