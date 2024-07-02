823f19e53c94b5394535eeaea3b6307ced354e46
yyang12302 <100991505+yyang12302@users.noreply.github.com>
2024-07-03 05:02:54 +0800

    Update get_ape_info.py

--- get_ape_info.py
+++ get_ape_info.py
@@ -17,7 +17,7 @@ with open('/home/codio/workspace/abi.json', 'r') as f:
 
 ############################
 #Connect to an Ethereum node
-api_url = "https://api.etherscan.io/api?module=contract&action=getabi&address=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"  #YOU WILL NEED TO TO PROVIDE THE URL OF AN ETHEREUM NODE
+api_url = "https://mainnet.infura.io/v3/aafeae9b6729474abd0aec8eaf878d77=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"  #YOU WILL NEED TO TO PROVIDE THE URL OF AN ETHEREUM NODE
 provider = HTTPProvider(api_url)
 web3 = Web3(provider)
 contract = web3.eth.contract(address=bayc_address, abi=abi)
