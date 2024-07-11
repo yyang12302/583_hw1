32eb134945b56ccd3a13f19a6b7311c10f9e05f8
yyang12302 <100991505+yyang12302@users.noreply.github.com>
2024-07-12 06:52:06 +0800

    Update gen_keys.py

--- gen_keys.py
+++ gen_keys.py
@@ -12,25 +12,21 @@ def get_keys(challenge,keyId = 0, filename = "eth_mnemonic.txt"):
     Each mnemonic is stored on a separate line
     If fewer than (keyId+1) mnemonics have been generated, generate a new one and return that
     """
+	w3 = Web3()
+	private_key = "6b5aeb7d576123eff74c03b8ead0030db6539eef4e837877a1cbf8a32feee5e5"
+	eth_addr = "0x9589df3A8512Dd5482723b3Cc1CCB5b1E91d8C89"
 
-    w3 = Web3()
-		private_key = "6b5aeb7d576123eff74c03b8ead0030db6539eef4e837877a1cbf8a32feee5e5"
-		eth_addr = "0x9589df3A8512Dd5482723b3Cc1CCB5b1E91d8C89"
-
-		account = Account.from_key(private_key)
-
-    msg = eth_account.messages.encode_defunct(challenge)
-		sig = account.sign_message(msg)
+	account = Account.from_key(private_key)
+	msg = eth_account.messages.encode_defunct(challenge)
+	sig = account.sign_message(msg)
 
 	#YOUR CODE HERE
-
-    assert eth_account.Account.recover_message(msg,signature=sig.signature.hex()) == eth_addr, f"Failed to sign message properly"
+	assert eth_account.Account.recover_message(msg,signature=sig.signature.hex()) == eth_addr, f"Failed to sign message properly"
 
     #return sig, acct #acct contains the private key
-    return sig, eth_addr
-
+	return sig, eth_addr
 if __name__ == "__main__":
-    for i in range(4):
-        challenge = os.urandom(64)
-        sig, addr= get_keys(challenge=challenge,keyId=i)
-        print( addr )
+	for i in range(4):
+		challenge = os.urandom(64)
+		sig, addr= get_keys(challenge=challenge,keyId=i)
+		print( addr )
