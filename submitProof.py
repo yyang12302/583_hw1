eb75076fd4291998beebb6988a4080126487b1f5
yyang12302 <100991505+yyang12302@users.noreply.github.com>
2024-07-19 08:59:00 +0800

    Update submitProof.py

--- submitProof.py
+++ submitProof.py
@@ -28,7 +28,7 @@ def merkle_assignment():
     tree = build_merkle(leaves)
 
     # Select a random leaf and create a proof for that leaf
-    random_leaf_index = 100 #TODO generate a random index from primes to claim (0 is already claimed)
+    random_leaf_index = 200 #TODO generate a random index from primes to claim (0 is already claimed)
     proof = prove_merkle(tree, random_leaf_index)
 
     # This is the same way the grader generates a challenge for sign_challenge()
